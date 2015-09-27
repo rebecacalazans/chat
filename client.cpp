1aae881ff7e1d03555993270af4491b0cd4dfe28
Rebeca Calazans <rebecacalazans@rebeca-linux.localdomain>
2015-09-27 03:47:15 -0300

    Adicionada interface em ncurses

--- client.cpp
+++ client.cpp
@@ -5,179 +5,186 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-#include <termios.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <thread>
 #include <mutex>
-#define MAX 1024
+#include <curses.h>
 
-int colorlist[] = {
-  31, // red
-  32, // green
-  33, // yellow
-  34, // blue
-  35, // magenta
-  36, // cyan
-  37, // light gray
-  90, // dark gray
-  91, // light red
-  92, // light green
-  93, // light yellow
-  94, // light blue
-  95, // light magenta
-  96 // light cyan
-};
+const int NAME_LEN = 24;
+const int MSG_LEN = 1000;
+const int PACKET_LEN = NAME_LEN+MSG_LEN+20;
 
-int color;
-char name[20];
-char message[MAX];
+char name[NAME_LEN];
+char msg[MSG_LEN];//Usado para enviar
+char packet[PACKET_LEN];//Usado pra receber
+int ncolums = 0, nlines = 0;
 
-std::mutex mutmessage;
+WINDOW *chatwin, *typewin;
 
-struct termios oldtio = {};
-void startInput() {
-  if (tcgetattr(0, &oldtio) < 0)
-    perror("tcsetattr()");
+std::mutex mutwin;
 
-  struct termios newtio = oldtio;
-  newtio.c_lflag &= ~ICANON;
-  newtio.c_lflag &= ~ECHO;
-  newtio.c_cc[VMIN] = 1;
-  newtio.c_cc[VTIME] = 0;
-  if (tcsetattr(0, TCSANOW, &newtio) < 0)
-    perror("tcsetattr ICANON");
+int  input(WINDOW *win, char *str, int len, bool cont) {
+  int i = 0;
+  while(1) {
+    int c = wgetch(win);
+    if(c == KEY_ENTER || c == 10) {
+      if (i > 0)
+        break;
+    }
+    else if (c == KEY_BACKSPACE) {
+      if (i > 0) {
+        str[--i] = '\0';
+        int y, x;
+        mutwin.lock();
+        getyx(win, y, x);
+        if (x > 0) {
+          mvwaddch(win, y, --x, ' ');
+        }
+        else if (y > 0) {
+          y--;
+          x = ncolums;
+          mvwaddch(win, y, --x, ' ');
+        }
+        wmove(win, y, x);
+        mutwin.unlock();
+      }
+    }
+    else if (!((265 <= c && 276 >=c) || (258 <= c && 261 >= c ) || (c == 27) || c == 9)) {
+      if (i < len) {
+        str[i++] = c;
+        waddch(win, c);
+      }
+    }
+    int x, y;
+
+    mutwin.lock();
+    getyx(win, y, x);
+    if (cont) {
+      wattron(win, COLOR_PAIR(2));
+      mvwprintw(win, 0, ncolums - 10, "%d/%d", i, len);
+      wattroff(win, COLOR_PAIR(2));
+    }
+    wmove(win, y,x);
+    wrefresh(win);
+    mutwin.unlock();
+  }
+  return (i);
 }
 
-void stopInput() {
-  if (tcsetattr(0, TCSANOW, &oldtio) < 0)
-    perror ("tcsetattr ~ICANON");
+void exit_(void) {
+  delwin(chatwin);
+  delwin(typewin);
+  endwin();
+  exit(0);
 }
 
 void send_thread(int sockfd) {
-  color = 0;
-  for (int i = 0; i < (int)strlen(name); ++i)
-    color += name[i];
-  color = (color % (sizeof(colorlist)/sizeof(int)));
-  color = colorlist[color];
-
-  char packet[MAX];
 
   while(1) {
-    mutmessage.lock();
-    message[0] = '\0';
-    mutmessage.unlock();
 
-    int i = 0;
+    wclear(typewin);
+    wattron(typewin, COLOR_PAIR(2));
+    mvwhline(typewin, 0, 0, ' ', ncolums);
+    mvwprintw(typewin, 0, ncolums - 10, "0/%d", MSG_LEN);
+    wattroff(typewin, COLOR_PAIR(2));
 
-    startInput();
-    while (1) {
-      char c = getchar();
-      if (c == '\n') {
-        if (i == 0) continue;
-        printf("\n");
-        break;
-      }
+    mutwin.lock();
+    mvwprintw(typewin, 1, 0, ">> ");
+    wrefresh(typewin);
+    mutwin.unlock();
 
-      if (c == 0x7f) { // Backspace
-        if (i > 0) {
-          mutmessage.lock();
-          message[--i] = '\0';
-          mutmessage.unlock();
+    memset(msg,0,MSG_LEN);
+    input(typewin, msg, MSG_LEN, true);
 
-          printf("\b \b");
-        }
-      } else if (c == 27) { // Escape sequence
-        // Ignore escape sequence
-        c = getchar();
-        c = getchar();
-        continue;
-      } else {
-        mutmessage.lock();
-        message[i++] = c;
-        message[i] = '\0';
-        mutmessage.unlock();
-        printf("%c", c);
-      }
-
-      if (i == MAX-1) {
-        mutmessage.lock();
-        message[i] = '\n';
-        mutmessage.unlock();
-        printf("\n");
-        break;
-      }
-    }
-    stopInput();
-
-    /*
-    struct winsize w;
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-    int meslines = (strlen(message) + w.ws_col - 1) / w.ws_col;
-    printf("\033[%dA", meslines);
-    */
-    printf("\033[1A");
-
-
-    int mlen = send(sockfd, message, strlen(message), 0);
+    int mlen = send(sockfd, msg, strlen(msg), 0);
     if (mlen == -1 || mlen == 0) {
       printf("Erro ao enviar mensagem");
-      exit (1);
+      exit_();
     }
   }
 }
 
 void rcv_thread(int sockfd) {
-  char* packet = (char*) malloc(MAX);
-  if (!packet) {
-    perror("Falha na alocação da memória");
-      exit (1);
-  }
 
   while(1) {
-    memset(packet, 0, MAX);
-    int mlen = recv(sockfd, packet, MAX, 0);
+    memset(packet, 0, PACKET_LEN);
+
+    int mlen = recv(sockfd, packet, PACKET_LEN, 0);
     if(mlen == -1 || mlen == 0) {
       perror("Erro ao receber mensagem\n");
-      exit (1);
+      exit_();
     }
     packet[mlen] = '\0';
+    int y,x;
 
-    // Get terminal size
-    struct winsize w;
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-    int col = w.ws_col - strlen(packet) + 9; // Get terminal size minus packet size (except escape sequences (9 chars))
-    /*
-    int meslines = (strlen(message) + w.ws_col - 1) / w.ws_col-1;
-    if (meslines > 0)
-      printf("\033[%dA", meslines);
-    */
+    getyx(typewin, y, x);
+    wprintw(chatwin, "%s\n", packet);
 
-    printf("\r%s", packet);
-    for (int i = 0; i < col; ++i) {
-      printf(" ");
-    }
-
-    printf("\n\033[%dm%s\033[0m> ", color, name);
-
-    mutmessage.lock();
-    printf("%s", message);
-    mutmessage.unlock();
-    fflush(stdout);
+    mutwin.lock();
+    wrefresh(chatwin);
+    wrefresh(typewin);
+    wmove(typewin, y,x);
+    mutwin.unlock();
   }
 }
 
+int sockfd;
 int main(int argc, char **argv) {
   std::thread tsend, trcv;
-  int sockfd;
   struct sockaddr_in addr;
 
+  //Iniciando curses
+  WINDOW *namewin;
+  initscr();
+  cbreak();
+  noecho();
+
+  start_color();
+
+  init_color(COLOR_BLACK,0,0,0);
+  init_pair(1,COLOR_WHITE,COLOR_BLACK);
+  init_pair(2,COLOR_BLACK,COLOR_WHITE);
+
+  bkgd(COLOR_PAIR(4));
+
+  getmaxyx(stdscr, nlines, ncolums);
+
+  chatwin = newwin(nlines-5, ncolums, 0, 0);
+  scrollok(chatwin, TRUE);
+  typewin = newwin(5, ncolums, nlines-5, 0);
+  keypad(typewin, TRUE);
+  namewin = newwin(5, NAME_LEN+4, nlines/2-2, (ncolums-NAME_LEN-4)/2);
+  keypad(namewin, TRUE);
+
+  wbkgd(chatwin,COLOR_PAIR(1));
+  wbkgd(typewin,COLOR_PAIR(1));
+  wbkgd(namewin,COLOR_PAIR(1));
+  wattron(namewin, A_BOLD);
+  wattron(chatwin, A_BOLD);
+
+  int y,x;
+  getmaxyx(namewin, y, x);
+
+  box(namewin, ACS_VLINE, ACS_HLINE);
+  mvwprintw(namewin, 1, x/2 - 9, "Digite seu nome:");
+  mvwhline(namewin, 3, 2, ' ', NAME_LEN);
+
+  memset(name, 0, NAME_LEN);
+  input(namewin, name, NAME_LEN, false);
+
+  delwin(namewin);
+  wrefresh(typewin);
+  wrefresh(chatwin);
+
+
+  //Iniciando conexão
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
   if(sockfd == -1) {
     perror("Erro ao criar socket");
-    return 1;
+    exit_();
   }
 
   addr.sin_family = AF_INET;
@@ -188,32 +195,26 @@ int main(int argc, char **argv) {
     addr.sin_addr.s_addr = inet_addr(argv[1]);
   memset(&addr.sin_zero,0,sizeof(addr.sin_zero));
 
-  printf("Digite seu nome: ");
-  fgets(name, 18, stdin);
-  name[strlen(name) - 1] = '\0';
 
   if(connect(sockfd,(struct sockaddr*)&addr,sizeof(addr)) != 0)
   {
-    printf("Erro ao se conectar!\n");
-    return 1;
+    printf("Erro ao conectar!\n");
+    exit_();
   }
   else {
     int mlen = send(sockfd, name, strlen(name), 0);
     if (mlen == -1 || mlen == 0) {
-      printf("Erro ao enviar mensagem");
-      exit (1);
+      wprintw(chatwin, "Erro ao enviar mensagem");
+      exit_();
     }
   }
 
-  tsend = std::thread(&send_thread, sockfd);
-  trcv = std::thread(&rcv_thread, sockfd);
+   tsend = std::thread(&send_thread, sockfd);
+   trcv = std::thread(&rcv_thread, sockfd);
 
-  printf("\033[%dm%s\033[0m> ", color, name);
-  fflush(stdout);
-
-  tsend.join();
-  trcv.join();
+   tsend.join();
+   trcv.join();
 
   close(sockfd);
-  return 0;
+  exit_();
 }
